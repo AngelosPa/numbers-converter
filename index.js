644af0395d54b203cb0f59495cfef6be74ecdb8f
angelo.p <mrpalaiologos@gmail.com>
2021-05-04 14:37:45 +0200

    basic layout ready

---
+++ index.js
@@ -0,0 +1,8 @@
+//from decimal to binary or from decimal to hexadecimal
+const year = new Date().getFullYear();
+document.querySelector(".year").innerHTML = year;
+let userNumber = 0;
+const result = document.querySelector(".result");
+const userInput = document.querySelector("#userInput");
+const currency = document.querySelector("#currency");
+const badUserInput = document.querySelector("#badUserInput");
