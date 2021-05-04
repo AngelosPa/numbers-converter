d5f2ebdff47a58bb1f1914978eb5c914eafc2342
angelo.p <mrpalaiologos@gmail.com>
2021-05-04 15:34:58 +0200

    almost biuld the converter

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ const year = new Date().getFullYear();
 document.querySelector(".year").innerHTML = year;
 let userNumber = 0;
 const result = document.querySelector(".result");
+console.log(result);
 const userInput = document.querySelector("#userInput");
-const currency = document.querySelector("#currency");
-const badUserInput = document.querySelector("#badUserInput");
+const numbi = document.querySelector("#numbi");
+//to get what the user gives
+function registerUserValue(e) {
+  console.log(e.target.value);
+  userNumber = e.target.value;
+}
+
+function convert() {
+  let converter = 0;
+
+  if (numbi.value == "decinal") {
+    result.innerHTML = "..";
+  } else if (numbi.value == "binary") {
+    result.innerHTML = "im binary";
+  } else result.innerHTML = "im a hex";
+}
+
+numbi.addEventListener("change", convert);
+userInput.addEventListener("keyup", registerUserValue);
